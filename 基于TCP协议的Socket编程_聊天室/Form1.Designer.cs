ba7d922696912c47cd6f4b716696788e4b8c4cb6
CXQ513 <qiqi9989@126.com>
2020-04-07 14:04:43 +0800

    合并分支1

--- 基于TCP协议的Socket编程_聊天室/Form1.Designer.cs
+++ 基于TCP协议的Socket编程_聊天室/Form1.Designer.cs
@@ -31,13 +31,13 @@
             this.txtServer = new System.Windows.Forms.TextBox();
             this.txtPort = new System.Windows.Forms.TextBox();
             this.btnStart = new System.Windows.Forms.Button();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cboUsers = new System.Windows.Forms.ComboBox();
             this.txtLog = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtMsg = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
@@ -47,7 +47,7 @@
             this.txtServer.Name = "txtServer";
             this.txtServer.Size = new System.Drawing.Size(100, 21);
             this.txtServer.TabIndex = 0;
-            this.txtServer.Text = "192.168.1.103";
+            this.txtServer.Text = "192.168.1.107";
             // 
             // txtPort
             // 
@@ -67,13 +67,13 @@
             this.btnStart.UseVisualStyleBackColor = true;
             this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
-            // comboBox1
+            // cboUsers
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(437, 48);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(152, 20);
-            this.comboBox1.TabIndex = 3;
+            this.cboUsers.FormattingEnabled = true;
+            this.cboUsers.Location = new System.Drawing.Point(437, 48);
+            this.cboUsers.Name = "cboUsers";
+            this.cboUsers.Size = new System.Drawing.Size(152, 20);
+            this.cboUsers.TabIndex = 3;
             // 
             // txtLog
             // 
@@ -83,13 +83,14 @@
             this.txtLog.Size = new System.Drawing.Size(531, 120);
             this.txtLog.TabIndex = 4;
             // 
-            // textBox4
+            // txtMsg
             // 
-            this.textBox4.Location = new System.Drawing.Point(58, 233);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(531, 120);
-            this.textBox4.TabIndex = 5;
+            this.txtMsg.Location = new System.Drawing.Point(58, 233);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(531, 120);
+            this.txtMsg.TabIndex = 5;
+            this.txtMsg.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMsg_KeyDown);
             // 
             // textBox5
             // 
@@ -104,7 +105,7 @@
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 7;
-            this.button2.Text = "button2";
+            this.button2.Text = "发送文件";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
@@ -113,17 +114,18 @@
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 8;
-            this.button3.Text = "button3";
+            this.button3.Text = "选择";
             this.button3.UseVisualStyleBackColor = true;
             // 
-            // button4
+            // btnSend
             // 
-            this.button4.Location = new System.Drawing.Point(481, 359);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 9;
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.btnSend.Location = new System.Drawing.Point(481, 359);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 9;
+            this.btnSend.Text = "发送消息";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // button5
             // 
@@ -131,7 +133,7 @@
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(75, 23);
             this.button5.TabIndex = 10;
-            this.button5.Text = "button5";
+            this.button5.Text = "震动";
             this.button5.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -140,18 +142,18 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(670, 450);
             this.Controls.Add(this.button5);
-            this.Controls.Add(this.button4);
+            this.Controls.Add(this.btnSend);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.txtMsg);
             this.Controls.Add(this.txtLog);
-            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.cboUsers);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.txtPort);
             this.Controls.Add(this.txtServer);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "服务器";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -163,13 +165,13 @@
         private System.Windows.Forms.TextBox txtServer;
         private System.Windows.Forms.TextBox txtPort;
         private System.Windows.Forms.Button btnStart;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cboUsers;
         private System.Windows.Forms.TextBox txtLog;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox txtMsg;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.Button button5;
     }
 }
--- 基于TCP协议的Socket编程_聊天室/Form1.cs
+++ 基于TCP协议的Socket编程_聊天室/Form1.cs
@@ -22,57 +22,87 @@ namespace 基于TCP协议的Socket编程_聊天室
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            //当点击开始监听的时候，在服务器端口创建一个负责监听的IP地址跟端口号的Socket
-            Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPAddress ip = IPAddress.Any;
-            //创建端口号对象
-            IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
-            //监听
-            socketWatch.Bind(point);
-            ShowMsg("监听成功");
-            socketWatch.Listen(10);
+            try
+            {
+                //当点击开始监听的时候，在服务器端口创建一个负责监听的IP地址跟端口号的Socket
+                Socket socketWatch = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPAddress ip = IPAddress.Any;
+                //创建端口号对象
+                IPEndPoint point = new IPEndPoint(ip, Convert.ToInt32(txtPort.Text));
+                //监听
+                socketWatch.Bind(point);
+                ShowMsg("监听成功");
+                socketWatch.Listen(10);
 
-            Thread th = new Thread(Listen);
-            th.IsBackground = true;
-            th.Start(socketWatch);
+                Thread th = new Thread(Listen);
+                th.IsBackground = true;
+                th.Start(socketWatch);
+            }
+            catch
+            { }
+
         }
 
         /// <summary>
         /// 等待客户端的连接，并且创建与之通信的Socket
         /// </summary>
+        /// 
+        Socket socketSend;
         void Listen(object o)
         {
             Socket socketWatch = o as Socket;
+            //等待客户端连接 并且创建一个负责通讯的Socket
             while (true)
             {
-                //等待客户端连接 并且创建一个负责通讯的Socket
-                Socket socketSend = socketWatch.Accept();
-                ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
-                //开启一个新线程，不停地接受客户端发来的消息
-                Thread th = new Thread(Receive);
-                th.IsBackground = true;
-                th.Start(socketSend);
+                try
+                {
+                    //负责跟客户端通信的Socket
+                    socketSend = socketWatch.Accept();
+                    //将远程连接的客户端的IP地址和Socket存入集合中
+                    dicScoket.Add(socketSend.RemoteEndPoint.ToString(), socketSend);
+                    //将远程连接的客户端的IP地址和端口号存入下拉框中
+                    cboUsers.Items.Add(socketSend.RemoteEndPoint.ToString());
+                    ShowMsg(socketSend.RemoteEndPoint.ToString() + ":" + "连接成功");
+                    //开启一个新线程，不停地接受客户端发来的消息
+                    Thread th = new Thread(Receive);
+                    th.IsBackground = true;
+                    th.Start(socketSend);
+                }
+                catch
+                { }
             }
         }
+
+        //将远程连接的客户端的IP地址和Socket存入集合中
+        Dictionary<string, Socket> dicScoket = new Dictionary<string, Socket>();
 
         /// <summary>
         /// 服务器端不停地接受客户端发来的消息
         /// </summary>
         /// <param name="o"></param>
-        void Receive (object o)
+        void Receive(object o)
         {
             Socket socketSend = o as Socket;
             while (true)
             {
-                //客户端连接成功后，服务器应该接受客户端发来的消息
-                byte[] buffer = new byte[1024 * 1024 * 2];
-                //实际接收到的有效字符
-                int r = socketSend.Receive(buffer);
-                string str = Encoding.UTF8.GetString(buffer, 0, r);
-                ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                try
+                {
+                    //客户端连接成功后，服务器应该接受客户端发来的消息
+                    byte[] buffer = new byte[1024 * 1024 * 2];
+                    //实际接收到的有效字符
+                    int r = socketSend.Receive(buffer);
+                    if (r == 0)
+                    {
+                        break;
+                    }
+                    string str = Encoding.UTF8.GetString(buffer, 0, r);
+                    ShowMsg(socketSend.RemoteEndPoint + ":" + str);
+                }
+                catch
+                { }
             }
         }
-        void ShowMsg (string str)
+        void ShowMsg(string str)
         {
             txtLog.AppendText(str + "\r\n");
         }
@@ -80,6 +110,36 @@ namespace 基于TCP协议的Socket编程_聊天室
         private void Form1_Load(object sender, EventArgs e)
         {
             Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        /// <summary>
+        /// 服务器给客户端发送消息
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string str = txtMsg.Text;
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(str);
+            //获得用户在下拉框中选择的IP地址
+            string ip = cboUsers.SelectedItem.ToString();
+            dicScoket[ip].Send(buffer);
+            //socketSend.Send(buffer);
+        }
+
+        /// <summary>
+        /// txtMsg热键：回车键
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtMsg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                btnSend_Click(null, null);
+                txtMsg.Clear();
+                txtMsg.Focus();
+            }
         }
     }
 }
