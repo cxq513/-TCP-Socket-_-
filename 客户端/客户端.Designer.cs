abc5c109ec6d00e8a6de6ed2f0cfb0f8b7437aab
CXQ513 <qiqi9989@126.com>
2020-04-08 10:45:17 +0800

    删除客户端无用的按钮

--- 客户端/客户端.Designer.cs
+++ 客户端/客户端.Designer.cs
@@ -28,62 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button5 = new System.Windows.Forms.Button();
             this.btnSend = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.textBox5 = new System.Windows.Forms.TextBox();
             this.txtMsg = new System.Windows.Forms.TextBox();
             this.txtLog = new System.Windows.Forms.TextBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.btnStart = new System.Windows.Forms.Button();
             this.txtPort = new System.Windows.Forms.TextBox();
             this.txtServer = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(402, 356);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 21;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
             // btnSend
             // 
-            this.btnSend.Location = new System.Drawing.Point(493, 356);
+            this.btnSend.Location = new System.Drawing.Point(526, 356);
             this.btnSend.Name = "btnSend";
             this.btnSend.Size = new System.Drawing.Size(75, 23);
             this.btnSend.TabIndex = 20;
             this.btnSend.Text = "发送";
             this.btnSend.UseVisualStyleBackColor = true;
             this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(402, 385);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 19;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(493, 385);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 18;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // textBox5
-            // 
-            this.textBox5.Location = new System.Drawing.Point(70, 387);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(315, 21);
-            this.textBox5.TabIndex = 17;
             // 
             // txtMsg
             // 
@@ -102,17 +63,9 @@
             this.txtLog.Size = new System.Drawing.Size(531, 120);
             this.txtLog.TabIndex = 15;
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(449, 45);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(152, 20);
-            this.comboBox1.TabIndex = 14;
-            // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(354, 42);
+            this.btnStart.Location = new System.Drawing.Point(366, 44);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(75, 23);
             this.btnStart.TabIndex = 13;
@@ -141,14 +94,9 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(670, 450);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.btnSend);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox5);
             this.Controls.Add(this.txtMsg);
             this.Controls.Add(this.txtLog);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.btnStart);
             this.Controls.Add(this.txtPort);
             this.Controls.Add(this.txtServer);
@@ -161,15 +109,9 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button btnSend;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox txtMsg;
         private System.Windows.Forms.TextBox txtLog;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.TextBox txtPort;
         private System.Windows.Forms.TextBox txtServer;
